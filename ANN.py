a3845570a5e578633a1f0bcb7a2f784fb037a13c
Samruddhi-saoji <90205915+Samruddhi-saoji@users.noreply.github.com>
2023-06-17 01:26:34 +0530

    more activations, added xavier initialisation

--- ANN.py
+++ ANN.py
@@ -3,11 +3,10 @@ from scipy.special import expit   #for sigmoid function
 
 class Layer:
     #attributes
-    #weights, bias arrays
     weights = None  
     bias = None #for each nueron in the layer
 
-    z = None    
+    z = None
     activations = None
 
     dW = 0 #dE/dW
@@ -18,16 +17,20 @@ class Layer:
         self.neurons = neurons #number of neurons in the layer
         self.g = activation_func #activation function for this layer
             #calling the activation function: self.g(x)
-            #sigmoid, ReLU, tanh or softmax 
-        
+            
         #assign the activation function derivative
         if self.g == sigmoid :
             self.g_derivative = sigmoid_derivative
         elif self.g == ReLU :
             self.g_derivative = ReLU_derivative
+        elif self.g == leaky_relu :
+            self.g_derivative = leaky_relu_derivative
+        elif self.g == tanh :
+            self.g_derivative = tanh_derivative
         else: #softmax
-            self.g_derivative = None
+            self.g_derivative = softmax_derivative
 
+        
 
 #activation functions and derivatives
 def sigmoid(x) :
@@ -41,17 +44,42 @@ def ReLU(x):
 
 def ReLU_derivative(x):
     return x > 0
-#returns false for negative numbers and 0
+#returns 0 for negative numbers and 0
+
+def leaky_relu(x, alpha=0.15):
+    return np.maximum(alpha*x, x)
+
+def leaky_relu_derivative(x, alpha=0.15):
+    return np.where(x > 0, 1, alpha)
 
 def softmax(x) :
     exp = np.exp(x - np.max(x))
     return exp / exp.sum(axis=0)
+
+def softmax_derivative(x):
+    softmax_output = softmax(x)
+
+    # Calculate the Jacobian matrix of the softmax function
+    n = softmax_output.shape[1]
+    jacobian_matrix = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                jacobian_matrix[i, j] = softmax_output[0, i] * (1 - softmax_output[0, i])
+            else:
+                jacobian_matrix[i, j] = -softmax_output[0, i] * softmax_output[0, j]
+
+    return jacobian_matrix
+#output shape = (n,n)
+
 
 def tanh(x) :
     return np.tanh(x)
 
 def tanh_derivative(x) :
     return (1 - np.power(np.tanh(x), 2))
+
 
 
 
@@ -72,20 +100,23 @@ class Network :
             layer = self.layers[i]
             prev_layer = self.layers[i-1]
 
-            layer.weights = np.random.rand(layer.neurons, prev_layer.neurons)  - 0.5
-            layer.bias = np.random.rand(layer.neurons, 1)  - 0.5
+            # Initialize weights using Xavier initialization
+            layer.weights = np.random.randn(layer.neurons, prev_layer.neurons) * np.sqrt(1 / prev_layer.neurons)
+            layer.bias = np.zeros((layer.neurons, 1)) #all biases are 0
 
     
     #training
     def train(self, X, Y, lr, epochs) : 
-        expected_output = one_hot(Y)
-        #input layer
-        self.layers[0].activations = X #input activations
-
-
+        #rows and columns
         #m = number of training observations
+        #r = #of input layer neurons
+        #n = number of classes = number of output layer neurons
         r,m = X.shape
 
+        expected_output = one_hot(Y) #shape = (n,m)
+        self.layers[0].activations = X #input activations
+
+    
         #training episodes
         for _ in range(epochs) :
             #### forward propagation ##########################
@@ -102,7 +133,7 @@ class Network :
             #loss function J = (predicted output - expected output)^2
             output_layer = self.layers[self.n-1]
             predicted_output = output_layer.activations
-            error = 2*(predicted_output - expected_output) 
+            error = 2*(predicted_output - expected_output) #shape = (n,m)
 
             #gradient for output layer
             #currently, prev_layer is the last hidden layer
@@ -110,22 +141,24 @@ class Network :
             output_layer.dB = np.sum(error, 1)/m 
 
             #calculate gradient for each of the hidden layers
-            for i in range(self.n-2, 0, -1) : #going from n-2 to 0 
+            for i in range(self.n-2, 0, -1) : #going from self.n-2 to 0 
                 prev_layer = self.layers[i-1]
                 layer = self.layers[i]
                 next_layer = self.layers[i+1]
 
                 #update the value of error for this layer
-                error = np.dot(next_layer.weights.T, error)*layer.g_derivative(layer.z)
+                error = np.dot(next_layer.weights.T, error)*layer.g_derivative(layer.z)  
                     #error = dot(next layer weights, next layer error) * g'(z)
 
                 #gradients
-                layer.dW = np.dot(error , prev_layer.activations.T) / m
+                layer.dW = np.dot(error, prev_layer.activations.T)/m  
                 layer.dB = np.sum(error, 1)/m
+
             
             #update the weights and biases
             for i in range(1, self.n):
                 layer = self.layers[i]
+
                 # formula: w = w - lr*dW
                 layer.weights -= lr*(layer.dW)
                 layer.bias -= (lr)*np.reshape(layer.dB, (layer.neurons, 1))
@@ -144,12 +177,14 @@ class Network :
 
             z = np.dot(layer.weights, activations) + layer.bias
             activations = layer.g(z)
-            #no need to store the activations, z for each layer
-            #z, activation of only the output layer matters
+            #dont store the A,z of all layers #only output layer matters
         #now, "activations" has the value of the output layer activations
 
+        # activations shape = (no of classes, no of test images)
+        # predicted, expected output shape = (no of test images, )
+
         #### calculate accuracy #######
-        predicted_output = np.argmax(activations, 0)
+        predicted_output = np.argmax(activations.T, axis=1)
 
         #print predicted , expected output
         print("Expected output: ", expected_output)
